4bd0aeb896b3ecb092d4098968e03960b0aeb75e
Raul Mansur <raulfmansur@hotmail.com>
2018-07-23 02:22:39 -0300

    functions added and use of siimple css framework

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {textInput: '', textOutput: '', mode: 'Encode'}
+    this.convertString = this.convertString.bind(this)
+    this.changeMode = this.changeMode.bind(this)
+  }
+
+  onChange = (e) => {
+      this.setState({ [e.target.name]: e.target.value })
+  }
+
+  convertString(){
+    let output = ''
+
+    if(this.state.mode)
+      output = btoa(this.state.textInput)
+    else
+      output = atob(this.state.textInput)
+
+    this.setState({textOutput: output})
+  }
+  changeMode(){
+    let newMode = ''
+
+    if(this.state.mode === 'Encode')
+      newMode = 'Decode'
+    else    
+      newMode = 'Encode'
+  
+    this.setState({mode: newMode})
+  }
+
   render() {
+    const {textInput} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <h1>String {this.state.mode}r</h1>
+
+        <label className="siimple-label">Input String:</label>
+        <br/>
+        <input type="text" id="textInput" name="textInput" value={textInput} onChange={this.onChange} className="siimple-input"/>
+        <br/>
+
+        <div className="siimple-btn siimple-btn--primary" onClick={this.changeMode}>MODE</div>
+        <div className="siimple-btn siimple-btn--success" onClick={this.convertString}>Converter</div>
+
+        <div id="textOutput">Output: 
+            <p>{this.state.textOutput}</p>
+        </div>
       </div>
-    );
+      );
   }
 }
 
